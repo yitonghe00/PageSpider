5a2c76bb28c95a1e2bd0c0ab00a2e1b3aa08fb77
Yitong He <yitonghe@usc.edu>
2018-09-10 00:49:19 -0700

    Initial commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,14 @@
+def load_url_from_file(file_path: str):
+    # TODO: add the code needed to read the text file with the urls in it
+    pass
+
+
+def load_page(url: str):
+    # TODO: add the code that reads the url
+    pass
+
+
+def scrape_page(page_contets: str):
+    # TODO: analyse the text
+    pass
+
